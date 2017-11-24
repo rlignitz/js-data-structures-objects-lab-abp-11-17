3c4fb5b183a1cee915c1f588994722f0796b0ecc
Shea Lignitz <rlignitz@gmail.com>
2017-11-24 18:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   let newDriver = Object.assign({}, driver);
-  delete newDriver.key;
+  delete newDriver[key];
   return newDriver;
 }
